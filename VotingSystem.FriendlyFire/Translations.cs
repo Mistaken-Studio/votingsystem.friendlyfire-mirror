b32f5c348240ff7fd9983a4c8aea8999ecfc02d0
Xname <xnameserver@o2.pl>
2022-10-09 23:15:26 +0200

    Change default Translations

--- VotingSystem.FriendlyFire/Translations.cs
+++ VotingSystem.FriendlyFire/Translations.cs
@@ -10,13 +10,13 @@ namespace Mistaken.VotingSystem.FriendlyFire
 {
     internal class Translations : ITranslation
     {
-        public string PlayerVerifiedVotingInfo { get; set; } = "Naciśnij klaiwsz '<color=blue>V</color>' aby oddać głos za <color=green>wyłączeniem</color> <color=yellow>ognia bratobójczego</color>";
+        public string PlayerVerifiedVotingInfo { get; set; } = "Naciśnij klawisz mówienia na radiu (domyślnie '<b><color=blue>V</color></b>') aby oddać głos za <color=green>wyłączeniem</color> <color=yellow>ognia bratobójczego</color>";
 
         public string PlayerVotedInfo { get; set; } = "Oddałeś głos za <color=green>wyłączeniem</color> <color=yellow>ognia bratobójczego</color> na tą rundę!";
 
         public string PlayerAlreadyVotedInfo { get; set; } = "Już oddałeś swój głos na tą rundę!";
 
-        public string VotingInfo { get; set; } = "Za <color=green>wyłączeniem</color> <color=yellow>ognia bratobójczego</color> w tej rundzie zagłosowało: <color=green>{0} na {1}</color>";
+        public string VotingInfo { get; set; } = "<br><size=120%>Za <color=green>wyłączeniem</color> <color=yellow>ognia bratobójczego</color> w tej rundzie zagłosowało: <color=green>{0} na {1} graczy</color></size>";
 
         public string RoundStartedFriendlyFireEnabledInfo { get; set; } = "W tej rundzie <color=yellow>ogień bratobójczy</color> jest <color=red>włączony</color>!";
 
